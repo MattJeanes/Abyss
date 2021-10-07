0188c445a46188c23f762f82cebfd7f391a5d3df
Matt Jeanes <matthew.jeanes@myairops.com>
2021-10-07 11:29:30 +0100

    Use threading and fix subscription id

--- Abyss.Web/Commands/Discord/ServerCommand.cs
+++ Abyss.Web/Commands/Discord/ServerCommand.cs
@@ -1,6 +1,8 @@
 ﻿using Abyss.Web.Data;
 using Abyss.Web.Managers.Interfaces;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +13,15 @@ namespace Abyss.Web.Commands.Discord
     public class ServerCommand : BaseCommand
     {
         private readonly IServerManager _serverManager;
+        private readonly ILogger<ServerCommand> _logger;
 
         public override string Command => "server";
         public override string Permission => Permissions.ServerManager;
 
-        public ServerCommand(IServiceProvider serviceProvider, IServerManager serverManager) : base(serviceProvider)
+        public ServerCommand(IServiceProvider serviceProvider, IServerManager serverManager, ILogger<ServerCommand> logger) : base(serviceProvider)
         {
             _serverManager = serverManager;
+            _logger = logger;
         }
 
         public override async Task ProcessMessage(MessageCreateEventArgs e, List<string> args)
@@ -55,10 +59,17 @@ namespace Abyss.Web.Commands.Discord
                 await e.Message.RespondAsync("Server not found");
                 return;
             }
-            await e.Message.RespondAsync($"Starting up {server.Name}");
+            var thread = await TryCreateThread(e, $"Starting up {server.Name}");
             await _serverManager.Start(server.Id.ToString(), async (logItem) =>
             {
-                await e.Message.RespondAsync(logItem.ToString());
+                if (thread == null)
+                {
+                    await e.Message.RespondAsync(logItem.ToString());
+                }
+                else
+                {
+                    await thread.SendMessageAsync(logItem.ToString());
+                }
             });
         }
 
@@ -71,11 +82,35 @@ namespace Abyss.Web.Commands.Discord
                 await e.Message.RespondAsync("Server not found");
                 return;
             }
-            await e.Message.RespondAsync($"Stopping {server.Name}");
+            var thread = await TryCreateThread(e, $"Stopping {server.Name}");
             await _serverManager.Stop(server.Id.ToString(), async (logItem) =>
             {
-                await e.Message.RespondAsync(logItem.ToString());
+                if (thread == null)
+                {
+                    await e.Message.RespondAsync(logItem.ToString());
+                }
+                else
+                {
+                    await thread.SendMessageAsync(logItem.ToString());
+                }
             });
+        }
+
+        private async Task<DiscordThreadChannel> TryCreateThread(MessageCreateEventArgs e, string name)
+        {
+            DiscordThreadChannel thread = null;
+            if (!e.Channel.IsPrivate)
+            {
+                try
+                {
+                    thread = await e.Message.CreateThreadAsync(name, DSharpPlus.AutoArchiveDuration.Hour);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Unable to create thread");
+                }
+            }
+            return thread;
         }
     }
 }
