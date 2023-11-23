3fa1b14e3568f7893a1ee194f9db47530c32b7e3
Matt Jeanes <mattjeanes23@gmail.com>
2023-11-23 13:52:03 +0000

    angular upgrade part 1

--- Abyss.Web/ClientApp/src/app/app-routing.module.ts
+++ Abyss.Web/ClientApp/src/app/app-routing.module.ts
@@ -47,7 +47,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes, {}),
   ],
   exports: [RouterModule],
 })
